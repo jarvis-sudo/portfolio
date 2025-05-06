4aa3234466e9b93d5148615cd4e57d0fc3ca578a
jarvis-sudo <yudhishtaryelishala@gmail.com>
2025-05-06 10:22:59 +0530

    vercel push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Clients from "@/components/Clients";
 import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import { socialMedia } from '@/data'
-import { div } from 'framer-motion/client'
 import React from 'react'
 import MagicButton from './MagicButton'
 import { FaLocationArrow } from 'react-icons/fa'
